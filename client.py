e55f4334cf58ac4fe4e0cec3ead7673e3aea048f
cafebazaar <cafebazaar@cafebazaars-MacBook-Pro-6.local>
2023-06-17 18:23:07 +0330

    start the project

--- client.py
+++ client.py
@@ -1,1 +1,54 @@
+# import libraries
+import socket
 
+# create client socket
+ClientMultiSocket = socket.socket()
+
+# define host and port 
+host = '127.0.0.1'
+port = 2006
+
+# waiting to be connected to the server
+print('Waiting for connection response')
+try:
+    ClientMultiSocket.connect((host, port))
+except socket.error as e:
+    print(str(e))
+
+# receive the message from the server
+res = ClientMultiSocket.recv(1024)
+
+# prompt
+USER_PROMPT = "\n\nHey there! You can write the following commands:\n" + "1. Create Account: Create Account [USERNAME] [PASSWORD]\n" + "2. Login: Login [USERNAME] [PASSWORD]\n" + "3. Get List of Online Users: Show Online Users\n" + "4. Send Message: Send [MESSAGE] to [USERNAME]\n" + "5. Create Group: Create Group [GROUP_NAME]\n" + "6. Add User to Group: Add [USER] to [GROUP_NAME]]\n" + "7. Remove User from Group: Remove [USER] from [GROUP_NAME]\n"
+
+# encode message
+def encode_message(inp):
+    if inp.lower().startswith("1") or inp.lower().startswith("create account"):
+        return "SUCCESS", inp + "###a"
+    elif inp.lower().startswith("2") or inp.lower().startswith("login"):
+        return "SUCCESS", inp + "###a"
+    elif inp.lower().startswith("3") or inp.lower().startswith("show"):
+        return "SUCCESS", inp + "###a"
+    elif inp.lower().startswith("4") or inp.lower().startswith("send"):
+        return "SUCCESS", inp + "###a"
+    elif inp.lower().startswith("5") or inp.lower().startswith("create group"):
+        return "SUCCESS", inp + "###a"
+    elif inp.lower().startswith("6") or inp.lower().startswith("add"):
+        return "SUCCESS", inp + "###a"
+    elif inp.lower().startswith("7") or inp.lower().startswith("remove"):
+        return "SUCCESS", inp + "###a"
+    return "FAILURE", "Please enter a valid input"
+    
+
+# send message to server regularly
+while True:
+    Input = input(USER_PROMPT)
+    print("\n")
+    flag, em = encode_message(Input)
+    if flag == "SUCCESS":
+        ClientMultiSocket.send(str.encode(em))
+        res = ClientMultiSocket.recv(1024)
+        print(res.decode('utf-8'))
+    else:
+        print(em)
+ClientMultiSocket.close()
--- server.py
+++ server.py
@@ -1,1 +1,66 @@
+# import libraries
+import socket
+import os
+from _thread import *
 
+# create socket
+ServerSideSocket = socket.socket()
+
+# define host and port
+host = '127.0.0.1'
+port = 2006
+
+# number of threads (number of connected clients)
+ThreadCount = 0
+
+# bind server to client
+try:
+    ServerSideSocket.bind((host, port))
+except socket.error as e:
+    print(str(e))
+
+# server is listening
+print('Socket is listening...')
+ServerSideSocket.listen(5)
+
+# handle request
+def handle_client_request(req):
+    req_type, req_parameters = req.split("###")
+    if req_type == "CREATE_ACCOUNT":
+        return req_type + "*" + req_parameters
+    elif req_type == "LOGIN":
+        return req_type + "*" + req_parameters
+    elif req_type == "SHOW_ONLINE_USERS":
+        return req_type + "*" + req_parameters
+    elif req_type == "SEND_MESSAGE":
+        return req_type + "*" + req_parameters
+    elif req_type == "CREATE_GROUP":
+        return req_type + "*" + req_parameters
+    elif req_type == "ADD_USER_TO_GROUP":
+        return req_type + "*" + req_parameters
+    elif req_type == "GENERATE_KEY":
+        return req_type + "*" + req_parameters
+    elif req_type == "REFRESH_KEY":
+        return req_type + "*" + req_parameters
+    elif req_type == "REMOVE_USER_FROM_GROUP":
+        return req_type + "*" + req_parameters
+    else:
+        return "ERROR: Please enter avalid request type."
+
+# handle client
+def multi_threaded_client(connection):
+    connection.send(str.encode('Server is working:'))
+    while True:
+        data = connection.recv(2048)
+        # response = 'Server message: ' + data.decode('utf-8')
+        if not data:
+            break
+        connection.sendall(str.encode(handle_client_request(data.decode('utf-8'))))
+    connection.close()
+while True:
+    Client, address = ServerSideSocket.accept()
+    print('Connected to: ' + address[0] + ':' + str(address[1]))
+    start_new_thread(multi_threaded_client, (Client, ))
+    ThreadCount += 1
+    print('Thread Number: ' + str(ThreadCount))
+ServerSideSocket.close()
